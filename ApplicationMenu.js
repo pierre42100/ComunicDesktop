e01f5211921f3d40ddff5a29a39a0b7309550567
Pierre HUBERT <pierre.git@communiquons.org>
2019-02-11 18:43:03 +0100

    Can enable dark mode

--- ApplicationMenu.js
+++ ApplicationMenu.js
@@ -15,27 +15,53 @@ const {app, Menu} = require('electron');
 module.exports.Get = function(window){
 
 	return Menu.buildFromTemplate([
+
+		//File menu
 		{
 			label: "File",
 			submenu: [
+
+				//Close app
 				{
 					label: "Quit",
 					click: () => {
 						app.quit();
 					}
 				}
+
 			]
 		},
 
+
+		//Settings menu
+		{
+			label: "Settings",
+			submenu: [
+
+				//Dark mode
+				{
+					label: "Toggle dark mode",
+					click: () => {
+						window.webContents.executeJavaScript("ComunicWeb.components.darkTheme.setEnabled(!ComunicWeb.components.darkTheme.isEnabled());");
+					}
+				}
+			]
+		},
+
+
+		//Advanced menu
 		{
 			label: "Advanced",
 			submenu: [
+
+				//Dev tools
 				{
 					label: "Toggle developer tools",
 					click: () => {
 						window.webContents.toggleDevTools();
 					}
 				}
+
 			]
 		}
 	])
